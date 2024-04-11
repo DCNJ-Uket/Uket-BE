cb757df8b6d046ed8b62b60214ab172cc6aff33f
0703kyj <kyj0703@konkuk.ac.kr>
2024-04-11 19:10:53 +0900

    test: TicketAppApiApplicationTests 임시 Disable 처리 - test용 property를 설정하지 않아 발생하는 에러때문에 임시 비활성화처리 - 추후 추가 예정

--- application/ticket-app-api/src/test/java/com/uket/app/ticket/api/TicketAppApiApplicationTests.java
+++ application/ticket-app-api/src/test/java/com/uket/app/ticket/api/TicketAppApiApplicationTests.java
@@ -1,8 +1,10 @@
 package com.uket.app.ticket.api;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled
 @SpringBootTest
 class TicketAppApiApplicationTests {
 
