b4ee17355681c0fb497c5a6f59e60a619437bba6
0703kyj <kyj0703@konkuk.ac.kr>
2024-04-24 14:27:58 +0900

    feat: ExceptionHandler 추가 - UnKnownException 처리 - AuthException 처리 - BaseException 처리

---
+++ application/ticket-app-api/src/main/java/com/uket/app/ticket/api/handler/WebExceptionHandler.java
@@ -0,0 +1,160 @@
+package com.uket.app.ticket.api.handler;
+
+import com.uket.core.dto.response.ErrorResponse;
+import com.uket.core.exception.BaseException;
+import com.uket.core.exception.ErrorCode;
+import com.uket.domain.auth.exception.AuthException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.ConstraintViolationException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.nio.CharBuffer;
+import java.security.InvalidParameterException;
+import java.util.Enumeration;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.ServletRequestBindingException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+@RequiredArgsConstructor
+@Slf4j
+@RestControllerAdvice
+public class WebExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> unKnownException(HttpServletRequest request,
+            Exception exception) {
+        return handleUnhandledException(request, exception);
+    }
+
+    @ExceptionHandler(AuthException.class)
+    ResponseEntity<ErrorResponse> handleAuthException(HttpServletRequest request,
+            AuthException exception) {
+        ErrorCode errorCode = exception.getErrorCode();
+        if (errorCode == ErrorCode.UNKNOWN_SERVER_ERROR) {
+            return handleUnhandledException(request, exception);
+        }
+
+        log.warn("[AuthException] {}: {}", errorCode.getCode(), errorCode.getMessage(), exception);
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .body(ErrorResponse.of(errorCode));
+    }
+
+    @ExceptionHandler(BaseException.class)
+    ResponseEntity<ErrorResponse> handleDomainException(HttpServletRequest request,
+            BaseException exception) {
+        ErrorCode errorCode = exception.getErrorCode();
+        if (errorCode == ErrorCode.UNKNOWN_SERVER_ERROR) {
+            return handleUnhandledException(request, exception);
+        }
+
+        log.warn("[BaseException] {}: {}", errorCode.getCode(), errorCode.getMessage(), exception);
+        return ResponseEntity
+                .badRequest()
+                .body(ErrorResponse.of(errorCode));
+    }
+
+    @ExceptionHandler(Throwable.class)
+    ResponseEntity<ErrorResponse> handleUnhandledException(HttpServletRequest request,
+            Throwable exception) {
+
+        StringBuilder dump = dumpRequest(request).append("\n ")
+                .append(getStackTraceAsString(exception));
+        log.error("[UnhandledException] {} \n", dump);
+
+        return ResponseEntity
+                .badRequest()
+                .body(ErrorResponse.of(ErrorCode.UNKNOWN_SERVER_ERROR));
+    }
+
+    @ExceptionHandler(value = {
+            HttpMessageNotReadableException.class,
+            InvalidParameterException.class,
+            ServletRequestBindingException.class,
+            MethodArgumentNotValidException.class,
+            ConstraintViolationException.class,
+            MethodArgumentTypeMismatchException.class,
+    })
+    ResponseEntity<ErrorResponse> handleValidateException(HttpServletRequest request,
+            Exception exception) {
+
+        log.warn("[InvalidParameterException]", exception);
+
+        return ResponseEntity
+                .badRequest()
+                .body(ErrorResponse.of(ErrorCode.INVALID_INPUT_VALUE));
+    }
+
+    private String getStackTraceAsString(Throwable throwable) {
+        StringWriter stringWriter = new StringWriter();
+        throwable.printStackTrace(new PrintWriter(stringWriter));
+        return stringWriter.toString();
+    }
+
+    private StringBuilder dumpRequest(HttpServletRequest request) {
+        StringBuilder dump = new StringBuilder("HttpRequest Dump:")
+                .append("\n  Remote Addr   : ").append(request.getRemoteAddr())
+                .append("\n  Protocol      : ").append(request.getProtocol())
+                .append("\n  Request Method: ").append(request.getMethod())
+                .append("\n  Request URI   : ").append(request.getRequestURI())
+                .append("\n  Query String  : ").append(request.getQueryString())
+                .append("\n  Parameters    : ");
+
+        Enumeration<String> parameterNames = request.getParameterNames();
+        while (parameterNames.hasMoreElements()) {
+            String name = parameterNames.nextElement();
+            dump.append("\n    ").append(name).append('=');
+            String[] parameterValues = request.getParameterValues(name);
+            for (String value : parameterValues) {
+                dump.append(value);
+            }
+        }
+
+        dump.append("\n  Headers       : ");
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String name = headerNames.nextElement();
+            dump.append("\n    ").append(name).append(":");
+            Enumeration<String> headerValues = request.getHeaders(name);
+            while (headerValues.hasMoreElements()) {
+                dump.append(headerValues.nextElement());
+            }
+        }
+
+        dump.append("\n  Body       : ");
+        if (request.getContentType() != null && request.getContentType().contains("application/x-www-form-urlencoded")) {
+            dump.append("\n    ").append("Body is not readable for 'application/x-www-form-urlencoded' type or has been read");
+        } else {
+            try {
+                dump.append("\n    ").append(readableToString(request.getReader()));
+            } catch (IOException ex) {
+                dump.append("\n    ").append("NOT_READABLE");
+            } catch (IllegalStateException ex) {
+                dump.append("\n    ").append("BODY_ALREADY_READ");
+            }
+        }
+
+        return dump;
+    }
+
+    public String readableToString(Readable readable) throws IOException {
+        StringBuilder stringBuilder = new StringBuilder();
+        CharBuffer buffer = CharBuffer.allocate(1024);
+
+        while (readable.read(buffer) != -1) {
+            buffer.flip();
+            stringBuilder.append(buffer);
+            buffer.clear();
+        }
+        return stringBuilder.toString();
+    }
+
+}
