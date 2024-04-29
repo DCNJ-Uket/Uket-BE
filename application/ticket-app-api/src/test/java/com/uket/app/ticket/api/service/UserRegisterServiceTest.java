2bca98a885ed130858502bea1a91a87ba0844a96
0703kyj <kyj0703@konkuk.ac.kr>
2024-04-29 15:09:17 +0900

    test: UserRegisterService 테스트 추가 - register메소드 관련 테스트 코드 추가

---
+++ application/ticket-app-api/src/test/java/com/uket/app/ticket/api/service/UserRegisterServiceTest.java
@@ -0,0 +1,138 @@
+package com.uket.app.ticket.api.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.uket.domain.auth.dto.response.AuthToken;
+import com.uket.domain.university.entity.University;
+import com.uket.domain.university.repository.UniversityRepository;
+import com.uket.domain.user.dto.CreateUserDetailsDto;
+import com.uket.domain.user.dto.CreateUserDto;
+import com.uket.domain.user.entity.UserDetails;
+import com.uket.domain.user.entity.Users;
+import com.uket.domain.user.enums.Platform;
+import com.uket.domain.user.enums.UserRole;
+import com.uket.domain.user.service.UserService;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Transactional
+class UserRegisterServiceTest {
+
+    private static final String UNIVERSITY_OUTSIDER = "외부인";
+    private static final String UNIVERSITY_KONKUK = "건국대학교";
+
+    @Autowired
+    UserRegisterService userRegisterService;
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private UniversityRepository universityRepository;
+
+    private Users user;
+
+    @BeforeEach
+    void beforeEach() {
+        CreateUserDto createUserDto = CreateUserDto.builder()
+                .name("test")
+                .role(UserRole.ROLE_USER)
+                .platform(Platform.KAKAO)
+                .platformId("1234")
+                .email("abc@naver.com")
+                .build();
+
+        user = userService.saveUser(createUserDto);
+
+        universityRepository.save(University.builder().name(UNIVERSITY_OUTSIDER).build());
+        universityRepository.save(University.builder().name(UNIVERSITY_KONKUK).build());
+    }
+
+    @Test
+    void 회원가입시_토큰을_반환한다() {
+
+        CreateUserDetailsDto createUserDetailsDto = CreateUserDetailsDto.builder()
+                .depositorName("홍길동")
+                .phoneNumber("01012341234")
+                .studentMajor("컴퓨터 공학부")
+                .studentCode("1234")
+                .build();
+
+        AuthToken authToken = userRegisterService.register(user.getId(), createUserDetailsDto, null);
+
+        assertThat(authToken.accessToken()).isNotNull();
+        assertThat(authToken.refreshToken()).isNotNull();
+        assertThat(authToken.isRegistered()).isTrue();
+    }
+
+    @Test
+    void 대학을_입력하지_않을_시_외부인으로_처리된다() {
+        CreateUserDetailsDto createUserDetailsDto = CreateUserDetailsDto.builder()
+                .depositorName("홍길동")
+                .phoneNumber("01012341234")
+                .studentMajor("컴퓨터 공학부")
+                .studentCode("1234")
+                .build();
+
+        userRegisterService.register(user.getId(), createUserDetailsDto, UNIVERSITY_KONKUK);
+        Users findUser = userService.findById(user.getId());
+
+        assertThat(findUser.getUniversity().getName()).isEqualTo(UNIVERSITY_KONKUK);
+    }
+
+    @Test
+    void 대학을_입력할_시_해당_대학으로_처리된다() {
+        CreateUserDetailsDto createUserDetailsDto = CreateUserDetailsDto.builder()
+                .depositorName("홍길동")
+                .phoneNumber("01012341234")
+                .studentMajor("컴퓨터 공학부")
+                .studentCode("1234")
+                .build();
+
+        userRegisterService.register(user.getId(), createUserDetailsDto, null);
+        Users findUser = userService.findById(user.getId());
+
+        assertThat(findUser.getUniversity().getName()).isEqualTo(UNIVERSITY_OUTSIDER);
+    }
+
+    @Test
+    void 회원가입이_완료되면_회원의_가입여부가_true가_된다() {
+        CreateUserDetailsDto createUserDetailsDto = CreateUserDetailsDto.builder()
+                .depositorName("홍길동")
+                .phoneNumber("01012341234")
+                .studentMajor("컴퓨터 공학부")
+                .studentCode("1234")
+                .build();
+
+        userRegisterService.register(user.getId(), createUserDetailsDto, null);
+        Users findUser = userService.findById(user.getId());
+
+        assertThat(findUser.getIsRegistered()).isTrue();
+    }
+
+    @Test
+    void 회원가입이_완료되면_회원의_세부정보가_저장된다() {
+        String depositorName = "홍길동";
+        String phoneNumber = "01012341234";
+        String major = "컴퓨터 공학부";
+        String code = "1234";
+
+        CreateUserDetailsDto createUserDetailsDto = CreateUserDetailsDto.builder()
+                .depositorName(depositorName)
+                .phoneNumber(phoneNumber)
+                .studentMajor(major)
+                .studentCode(code)
+                .build();
+
+        userRegisterService.register(user.getId(), createUserDetailsDto, null);
+        Users findUser = userService.findById(user.getId());
+        UserDetails userDetails = findUser.getUserDetails();
+
+        assertThat(userDetails.getDepositorName()).isEqualTo(depositorName);
+        assertThat(userDetails.getPhoneNumber()).isEqualTo(phoneNumber);
+        assertThat(userDetails.getStudentMajor()).isEqualTo(major);
+        assertThat(userDetails.getStudentCode()).isEqualTo(code);
+    }
+}
