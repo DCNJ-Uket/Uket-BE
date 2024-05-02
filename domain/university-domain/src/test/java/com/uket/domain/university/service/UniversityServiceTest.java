2b8cd44d0430f4d2892c8cd365c37eaed6b1e58f
0703kyj <kyj0703@konkuk.ac.kr>
2024-05-02 18:55:04 +0900

    test: UniversityService getCurrentEvent 관련 테스트 추가

--- domain/university-domain/src/test/java/com/uket/domain/university/service/UniversityServiceTest.java
+++ domain/university-domain/src/test/java/com/uket/domain/university/service/UniversityServiceTest.java
@@ -1,10 +1,12 @@
 package com.uket.domain.university.service;
 
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 import com.uket.core.exception.ErrorCode;
+import com.uket.domain.university.entity.University;
 import com.uket.domain.university.exception.UniversityException;
 import com.uket.domain.university.repository.UniversityRepository;
 import java.util.Optional;
@@ -27,8 +29,46 @@ class UniversityServiceTest {
     void 기본값이_존재하지_않을_경우_예외를_발생시킨다() {
         when(universityRepository.findByName(any())).thenReturn(Optional.empty());
 
-        Assertions.assertThatThrownBy(() -> universityService.getDefault())
+        assertThatThrownBy(() -> universityService.getDefault())
                 .hasMessage(ErrorCode.NOT_FOUND_UNIVERSITY.getMessage())
                 .isInstanceOf(UniversityException.class);
+    }
+
+    @Test
+    void 요청한_대학이_존재하지_않는_경우_예외를_발생시킨다() {
+
+        when(universityRepository.findByName(any())).thenReturn(Optional.empty());
+
+        assertThatThrownBy(() -> universityService.getCurrentEvent(""))
+                .hasMessage(ErrorCode.NOT_FOUND_UNIVERSITY.getMessage())
+                .isInstanceOf(UniversityException.class);
+    }
+
+    @Test
+    void 요청한_대학이_일반인인_경우_예외를_발생시킨다() {
+
+        assertThatThrownBy(() -> universityService.getCurrentEvent("일반인"))
+                .hasMessage(ErrorCode.NOT_FOUND_UNIVERSITY.getMessage())
+                .isInstanceOf(UniversityException.class);
+    }
+
+    @Test
+    void 요청한_대학이_진행중인_축제가_없는_경우_null을_반환한다() {
+
+        when(universityRepository.findByName(any())).thenReturn(
+                Optional.of(University.builder().name("건국대학교").build())
+        );
+
+        Assertions.assertThat(universityService.getCurrentEvent("건국대학교")).isEmpty();
+    }
+
+    @Test
+    void 요청한_대학이_진행중인_축제가_있는_경우_축제의_id를_반환한다() {
+
+        when(universityRepository.findByName(any())).thenReturn(
+                Optional.of(University.builder().name("건국대학교").currentEvent(1L).build())
+        );
+
+        Assertions.assertThat(universityService.getCurrentEvent("건국대학교")).isNotEmpty();
     }
 }
