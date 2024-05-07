8c04ea60e654e46e3f770f2c6d1f34b025853c82
0703kyj <kyj0703@konkuk.ac.kr>
2024-05-07 10:56:33 +0900

    docs: login error response 추가

--- application/ticket-app-api/src/main/java/com/uket/app/ticket/api/controller/AuthApi.java
+++ application/ticket-app-api/src/main/java/com/uket/app/ticket/api/controller/AuthApi.java
@@ -4,8 +4,13 @@ import com.uket.app.ticket.api.dto.request.LoginRequest;
 import com.uket.app.ticket.api.dto.request.TokenReissueRequest;
 import com.uket.app.ticket.api.dto.response.AuthResponse;
 import com.uket.app.ticket.api.dto.response.TokenResponse;
+import com.uket.core.dto.response.ErrorResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +26,25 @@ import org.springframework.web.bind.annotation.RestController;
 public interface AuthApi {
 
     @Operation(summary = "소셜 로그인", description = "소셜 로그인을 진행합니다.")
+    @ApiResponse(responseCode = "400", description = "BAD REQUEST", content = @Content(
+            mediaType = "application/json",
+            examples = {
+                    @ExampleObject(name = "CM0002", description = "잘못된 플랫폼을 입력할 시 발생합니다.",
+                            value = """
+                            {"code": "CM0002", "message": "유효하지 않은 입력입니다."}
+                            """
+                    ),
+                    @ExampleObject(name = "AU0006", description = "지원하지 않는 플랫폼을 입력할 시 발생합니다.",
+                            value = """
+                            {"code": "AU0006", "message": "유효하지 않은 플랫폼입니다."}
+                            """
+                    ),
+                    @ExampleObject(name = "AU0002", description = "OAuth2 서버와 통신이 실패할 경우 발생합니다.",
+                            value = """
+                            {"code": "AU0002", "message": "OAuth2 요청이 실패했습니다."}
+                            """
+                    )
+            }, schema = @Schema(implementation = ErrorResponse.class)))
     @PostMapping("/login/{provider}")
     ResponseEntity<AuthResponse> login(
             @Valid
