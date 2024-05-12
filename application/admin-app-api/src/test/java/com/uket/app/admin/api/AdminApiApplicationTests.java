23a88240ecd988e8fdf8160061d5f67ef1545f97
0703kyj <kyj0703@konkuk.ac.kr>
2024-05-12 18:55:28 +0900

    feat: Admin application 추가

---
+++ application/admin-app-api/src/test/java/com/uket/app/admin/api/AdminApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uket.app.admin.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
