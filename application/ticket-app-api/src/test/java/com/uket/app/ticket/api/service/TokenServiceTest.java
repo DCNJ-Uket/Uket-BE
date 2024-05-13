8010f21d3ca88bf6a5e0632b3cac7a16b698da03
sam971114 <simonana@naver.com>
2024-05-14 05:33:22 +0900

    feat: test profile 분리

--- application/ticket-app-api/src/test/java/com/uket/app/ticket/api/service/TokenServiceTest.java
+++ application/ticket-app-api/src/test/java/com/uket/app/ticket/api/service/TokenServiceTest.java
@@ -14,11 +14,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 
 @SpringBootTest
 @Transactional
+@ActiveProfiles("test")
 class TokenServiceTest {
     @Autowired
     private TokenService tokenService;
