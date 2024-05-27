76117d85166d401d601e7fb3c64302a568f5537a
sam971114 <simonana@naver.com>
2024-05-27 11:14:30 +0900

    feat: refreshToken expiration 중복 설정 삭제

--- application/ticket-app-api/src/main/java/com/uket/app/ticket/api/controller/impl/DevController.java
+++ application/ticket-app-api/src/main/java/com/uket/app/ticket/api/controller/impl/DevController.java
@@ -43,7 +43,7 @@ public class DevController implements DevApi {
                 userDto.name(),
                 userDto.role(),
                 isRegistered);
-        String newRefreshToken = jwtAuthTokenUtil.createRefreshToken( 7_200_000L);
+        String newRefreshToken = jwtAuthTokenUtil.createRefreshToken( );
 
         AuthToken authToken = AuthToken.of(newAccessToken, newRefreshToken, isRegistered);
         return ResponseEntity.ok(TokenResponse.from(authToken));
