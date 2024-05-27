a306c4fed0d53ff7def5054a63305d879e43b1ec
0703kyj <kyj0703@konkuk.ac.kr>
2024-05-27 15:51:14 +0900

    feat: 티켓 발급 완료 시 response 설정 추가

--- domain/ticket-domain/src/main/java/com/uket/domain/ticket/dto/TicketDto.java
+++ domain/ticket-domain/src/main/java/com/uket/domain/ticket/dto/TicketDto.java
@@ -3,10 +3,11 @@ package com.uket.domain.ticket.dto;
 import com.uket.domain.ticket.entity.Ticket;
 
 public record TicketDto(
-
+        Long userId,
+        Long ticketId
 ) {
 
     public static TicketDto from(Ticket ticket) {
-        return null;
+        return new TicketDto(ticket.getUser().getId(), ticket.getId());
     }
 }
