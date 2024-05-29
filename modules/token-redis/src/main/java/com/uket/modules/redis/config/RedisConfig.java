cd6870da011732f7ac2c2538aed7d50c9df455e9
sam971114 <simonana@naver.com>
2024-05-29 09:51:24 +0900

    feat: RedisConfig import 수정

--- modules/token-redis/src/main/java/com/uket/modules/redis/config/RedisConfig.java
+++ modules/token-redis/src/main/java/com/uket/modules/redis/config/RedisConfig.java
@@ -2,8 +2,6 @@ package com.uket.modules.redis.config;
 
 import com.uket.modules.redis.properties.RedisProperties;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
