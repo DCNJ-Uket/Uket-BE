b7a328068c6f9aab3e26af611cf4f66bdd6c8f1f
Youngjun Kim <127181370+0703kyj@users.noreply.github.com>
2024-05-30 14:26:40 +0900

    hotfix: TimeZone설정을 위한 PostConstuct 추가 (#26)
    * hotfix: Spring TimeZone설정
    
    * hotfix: Spring TimeZone설정

--- application/ticket-app-api/src/main/java/com/uket/TicketApiApplication.java
+++ application/ticket-app-api/src/main/java/com/uket/TicketApiApplication.java
@@ -1,5 +1,7 @@
 package com.uket;
 
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
@@ -12,4 +14,8 @@ public class TicketApiApplication {
         SpringApplication.run(TicketApiApplication.class, args);
     }
 
+    @PostConstruct
+    public void init(){
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 }
