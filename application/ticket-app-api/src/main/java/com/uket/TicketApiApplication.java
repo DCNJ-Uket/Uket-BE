02f0f3bf3ab5b08220ecbf9d0bd1f5c75271e8f1
0703kyj <kyj0703@konkuk.ac.kr>
2024-05-30 15:23:01 +0900

    hotfix: Spring TimeZone설정 변경

--- application/ticket-app-api/src/main/java/com/uket/TicketApiApplication.java
+++ application/ticket-app-api/src/main/java/com/uket/TicketApiApplication.java
@@ -1,7 +1,5 @@
 package com.uket;
 
-import jakarta.annotation.PostConstruct;
-import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
@@ -12,10 +10,5 @@ public class TicketApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TicketApiApplication.class, args);
-    }
-
-    @PostConstruct
-    public void init(){
-        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
     }
 }
