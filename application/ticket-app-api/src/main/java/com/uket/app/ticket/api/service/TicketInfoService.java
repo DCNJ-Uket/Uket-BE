92a1ef3c3d52ba1c605a29a28336886d7186e733
0703kyj <kyj0703@konkuk.ac.kr>
2024-06-12 09:58:27 +0900

    hotfix: lazy init 문제 해결

--- application/ticket-app-api/src/main/java/com/uket/app/ticket/api/service/TicketInfoService.java
+++ application/ticket-app-api/src/main/java/com/uket/app/ticket/api/service/TicketInfoService.java
@@ -7,10 +7,12 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
+@Transactional(readOnly = true)
 public class TicketInfoService {
     private final TicketService ticketService;
     public List<CheckTicketDto> getUserTickets(Long userId) {
