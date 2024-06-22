21373c52036518a7d356fead3611d3ce1bb7b659
Youngjun Kim <127181370+0703kyj@users.noreply.github.com>
2024-06-22 21:28:04 +0900

    feat: 티켓 상태명 한글화 (#36)

--- domain/ticket-domain/src/main/java/com/uket/domain/ticket/enums/TicketStatus.java
+++ domain/ticket-domain/src/main/java/com/uket/domain/ticket/enums/TicketStatus.java
@@ -1,5 +1,16 @@
 package com.uket.domain.ticket.enums;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum TicketStatus {
-    BEFORE_ENTER, FINISH_ENTER, BEFORE_PAYMENT, RESERVATION_CANCEL, EXPIRED
+    BEFORE_ENTER("예매 완료"),
+    FINISH_ENTER("입장 완료"),
+    BEFORE_PAYMENT("입금 확인중"),
+    RESERVATION_CANCEL("예매 취소"),
+    EXPIRED("기간 만료");
+
+    private final String value;
 }
