53ddd8d89d45662cbe6294f2468730d0b8681826
sam971114 <simonana@naver.com>
2024-06-26 20:06:19 +0900

    feat: @AfterEach rollback

--- application/ticket-app-api/src/test/java/com/uket/app/ticket/api/service/TicketServiceTest.java
+++ application/ticket-app-api/src/test/java/com/uket/app/ticket/api/service/TicketServiceTest.java
@@ -51,6 +51,13 @@ class TicketServiceTest {
         testUser = userService.saveUser(createUserDto);
     }
 
+
+    @AfterEach
+    void afterEach() {
+        ticketRepository.deleteAll();
+        userRepository.deleteAll();
+    }
+
     @Test
     void 티켓이_성공적으로_삭제된다() {
         Ticket ticket = ticketRepository.save(Ticket.builder()
