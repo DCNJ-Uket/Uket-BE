6a81c5d139912d7582b8986f39fa431e7102379b
0703kyj <kyj0703@konkuk.ac.kr>
2024-06-30 18:44:41 +0900

    feat: 예매 플로우에 대학 인증 및 사용자 구분에 따른 예매 가능 시간 조회 분기 처리 추가 - 재학생의 경우 재학생 zone으로 처리 - 일반인의 경우 일반인 zone으로 처리

--- application/ticket-app-api/src/main/java/com/uket/app/ticket/api/controller/impl/EventController.java
+++ application/ticket-app-api/src/main/java/com/uket/app/ticket/api/controller/impl/EventController.java
@@ -5,6 +5,7 @@ import com.uket.app.ticket.api.dto.response.ShowResponse;
 import com.uket.app.ticket.api.dto.response.ReservationResponse;
 import com.uket.domain.event.dto.ShowDto;
 import com.uket.domain.event.dto.ReservationDto;
+import com.uket.domain.event.enums.ReservationUserType;
 import com.uket.domain.event.service.EventService;
 import com.uket.domain.event.service.ShowService;
 import com.uket.domain.event.service.ReservationService;
@@ -22,20 +23,23 @@ public class EventController implements EventApi {
     private final ReservationService reservationService;
 
     @Override
-    public ResponseEntity<ShowResponse> getShows(Long eventId) {
+    public ResponseEntity<ShowResponse> getShows(Long userId, Long eventId) {
 
         String universityName = eventService.findUniversityNameByEventId(eventId);
+        ReservationUserType reservationUserType = eventService.getReservationUserTypeByUniversityName(userId, universityName);
+
         List<ShowDto> shows = showService.findByEventId(eventId);
 
-        ShowResponse response = ShowResponse.of(universityName, shows);
+        ShowResponse response = ShowResponse.of(reservationUserType, universityName, shows);
         return ResponseEntity.ok(response);
     }
 
     @Override
-    public ResponseEntity<ReservationResponse> getPossibleReservations(Long showId) {
+    public ResponseEntity<ReservationResponse> getPossibleReservations(Long showId, String userType) {
 
         String showName = showService.findNameById(showId);
-        List<ReservationDto> reservations = reservationService.findByShowId(showId);
+        ReservationUserType reservationUserType = ReservationUserType.fromString(userType);
+        List<ReservationDto> reservations = reservationService.findByShowIdAndReservationUserType(showId,reservationUserType);
 
         ReservationResponse response = ReservationResponse.of(showName, reservations);
         return ResponseEntity.ok(response);
