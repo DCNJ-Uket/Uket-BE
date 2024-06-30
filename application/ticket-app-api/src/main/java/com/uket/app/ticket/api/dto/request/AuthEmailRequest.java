a1f32c521dddb5c943138262a6500bfde4024768
0703kyj <kyj0703@konkuk.ac.kr>
2024-06-30 17:12:29 +0900

    docs: 메일 관련 API 설명 추가

--- application/ticket-app-api/src/main/java/com/uket/app/ticket/api/dto/request/AuthEmailRequest.java
+++ application/ticket-app-api/src/main/java/com/uket/app/ticket/api/dto/request/AuthEmailRequest.java
@@ -1,11 +1,14 @@
 package com.uket.app.ticket.api.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 
 public record AuthEmailRequest(
         @Email
+        @Schema(description = "인증하고자 하는 이메일, ", example = "abc1234@konkuk.ac.kr")
         String email,
 
+        @Schema(description = "인증 하고자 하는 대학 ID(건국대학교의 경우 2)", example = "2")
         Long universityId
 ) {
 
