87bf2cea06504a5a92cccfe585437d2b2d2e8df7
Youngjun Kim <127181370+0703kyj@users.noreply.github.com>
2024-07-05 17:46:27 +0900

    feat: 예매 플로우에 대학 인증 및 사용자 구분에 따른 예매 가능 시간 조회 분기 처리 추가 (#42)
    - 재학생의 경우 재학생 zone으로 처리
    - 일반인의 경우 일반인 zone으로 처리

--- application/ticket-app-api/src/main/java/com/uket/app/ticket/api/dto/response/ShowResponse.java
+++ application/ticket-app-api/src/main/java/com/uket/app/ticket/api/dto/response/ShowResponse.java
@@ -1,14 +1,16 @@
 package com.uket.app.ticket.api.dto.response;
 
 import com.uket.domain.event.dto.ShowDto;
+import com.uket.domain.event.enums.ReservationUserType;
 import java.util.List;
 
 public record ShowResponse(
+        String reservationUserType,
         String universityName,
         List<ShowDto> shows
 ) {
 
-    public static ShowResponse of(String university, List<ShowDto> shows) {
-        return new ShowResponse(university, shows);
+    public static ShowResponse of(ReservationUserType reservationUserType, String university, List<ShowDto> shows) {
+        return new ShowResponse(reservationUserType.getValue(), university, shows);
     }
 }
--- domain/event-domain/src/main/java/com/uket/domain/event/repository/ReservationRepository.java
+++ domain/event-domain/src/main/java/com/uket/domain/event/repository/ReservationRepository.java
@@ -1,12 +1,13 @@
 package com.uket.domain.event.repository;
 
 import com.uket.domain.event.entity.Reservation;
-import java.time.LocalDateTime;
+import com.uket.domain.event.enums.ReservationUserType;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReservationRepository extends JpaRepository<Reservation,Long> {
 
     <T> List<T> findByShowId(Long showId, Class<T> type);
+    <T> List<T> findByShowIdAndType(Long showId, ReservationUserType reservationUserType, Class<T> type);
 
 }
