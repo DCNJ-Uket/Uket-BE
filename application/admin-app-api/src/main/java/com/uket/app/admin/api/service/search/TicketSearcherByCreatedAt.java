c87165213b7a5922449ec9507f346b5ee8f8724c
Youngjun Kim <127181370+0703kyj@users.noreply.github.com>
2024-10-26 22:14:22 +0900

    Revert "feat: Service 코드 lazy loading error fix (#66)" (#67)
    This reverts commit 46ed1a2b7e62970330664f191f3a3dbb74d30193.

--- application/admin-app-api/src/main/java/com/uket/app/admin/api/service/search/TicketSearcherByCreatedAt.java
+++ application/admin-app-api/src/main/java/com/uket/app/admin/api/service/search/TicketSearcherByCreatedAt.java
@@ -15,7 +15,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional(readOnly = true)
 public class TicketSearcherByCreatedAt extends TicketSearcher{
 
     public TicketSearcherByCreatedAt(TicketRepository ticketRepository) {
