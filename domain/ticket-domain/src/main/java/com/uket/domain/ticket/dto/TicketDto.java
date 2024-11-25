b1444d728dda27946d00c82ac5b1e988de76a9b2
nonaninona <starcraft0529@gmail.com>
2024-11-25 23:42:45 +0900

    feat : TicketingResponse에 depositUrl 추가를 위해, TicketDto에 depositUrl 추가(builder set 구문에 추가)

--- domain/ticket-domain/src/main/java/com/uket/domain/ticket/dto/TicketDto.java
+++ domain/ticket-domain/src/main/java/com/uket/domain/ticket/dto/TicketDto.java
@@ -1,5 +1,6 @@
 package com.uket.domain.ticket.dto;
 
+import com.uket.domain.event.entity.Events;
 import com.uket.domain.ticket.entity.Ticket;
 import com.uket.domain.ticket.enums.TicketStatus;
 import lombok.Builder;
@@ -10,16 +11,20 @@ public record TicketDto(
         Long userId,
         String userName,
         TicketStatus status,
-        String msg
+        String msg,
+        String depositUrl
 ) {
 
     public static TicketDto from(Ticket ticket) {
+        Events events = ticket.getEvent();
+
         return TicketDto.builder()
                 .ticketId(ticket.getId())
                 .userId(ticket.getUser().getId())
                 .userName(ticket.getUser().getName())
                 .status(ticket.getStatus())
                 .msg(ticket.getStatus().getMsg())
+                .depositUrl(events.getDepositUrl())
                 .build();
     }
 }
