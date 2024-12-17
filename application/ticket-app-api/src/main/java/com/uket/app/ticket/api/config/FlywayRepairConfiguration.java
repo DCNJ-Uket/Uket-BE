2a78d0a70ddaccc65a830812ac460f959852fc8f
sam971114 <simonana@naver.com>
2024-12-17 20:15:39 +0900

    feat: flyway migration 작업전 repair 설정

---
+++ application/ticket-app-api/src/main/java/com/uket/app/ticket/api/config/FlywayRepairConfiguration.java
@@ -0,0 +1,17 @@
+package com.uket.app.ticket.api.config;
+
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FlywayRepairConfiguration {
+
+    @Bean
+    public FlywayMigrationStrategy flywayMigrationStrategy() {
+        return flyway -> {
+            flyway.repair();
+            flyway.migrate();
+        };
+    }
+}
