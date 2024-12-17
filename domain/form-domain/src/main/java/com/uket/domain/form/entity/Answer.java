9652115dfc2b269d0587019f68c701d88dd84b1f
nonaninona <starcraft0529@gmail.com>
2024-12-17 20:29:10 +0900

    feat : Form을 JPA 엔티티로 변경

--- domain/form-domain/src/main/java/com/uket/domain/form/entity/Answer.java
+++ domain/form-domain/src/main/java/com/uket/domain/form/entity/Answer.java
@@ -3,14 +3,19 @@ package com.uket.domain.form.entity;
 import com.uket.domain.core.entity.BaseEntity;
 import com.uket.domain.user.entity.Users;
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 public abstract class Answer extends BaseEntity {
 
--- domain/form-domain/src/main/java/com/uket/domain/form/entity/Form.java
+++ domain/form-domain/src/main/java/com/uket/domain/form/entity/Form.java
@@ -1,12 +1,31 @@
 package com.uket.domain.form.entity;
 
+import com.uket.domain.core.entity.BaseEntity;
 import com.uket.domain.user.entity.Users;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-public abstract class Form {
+public abstract class Form extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "form_id")
     private Long id;
+
+    @Enumerated(value = EnumType.STRING)
     private FormType formType;
+
     private String question;
 
     abstract Answer createAnswer(Users user, String response);
