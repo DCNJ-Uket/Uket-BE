fc9c6eb2513548c63d7529e1c57c740ef5d186e3
nonaninona <starcraft0529@gmail.com>
2024-12-17 20:41:57 +0900

    feat : survey에서 event로 many to one 관계 추가

--- domain/form-domain/src/main/java/com/uket/domain/form/entity/Survey.java
+++ domain/form-domain/src/main/java/com/uket/domain/form/entity/Survey.java
@@ -2,14 +2,18 @@ package com.uket.domain.form.entity;
 
 import com.uket.core.exception.ErrorCode;
 import com.uket.domain.core.entity.BaseEntity;
+import com.uket.domain.event.entity.Events;
 import com.uket.domain.form.dto.UserResponseDto;
 import com.uket.domain.form.exception.FormException;
 import com.uket.domain.user.entity.Users;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
@@ -31,6 +35,10 @@ public class Survey extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "survey_id")
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "event_id")
+    private Events event;
 
     @OneToMany(mappedBy = "form_id")
     private List<Form> forms;
