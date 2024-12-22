482c598f280cd1a3acf2cc1e41b332e8158d84d2
0703kyj <kyj0703@konkuk.ac.kr>
2024-12-22 16:33:51 +0900

    [SVR-57] 베너 RedirectUrl 추가

--- domain/event-domain/src/main/java/com/uket/domain/event/dto/BannerDto.java
+++ domain/event-domain/src/main/java/com/uket/domain/event/dto/BannerDto.java
@@ -2,10 +2,11 @@ package com.uket.domain.event.dto;
 
 public record BannerDto(
         String title,
-        String url
+        String url,
+        String redirectUrl
 ) {
 
-    public static BannerDto of(String title, String bannerUrl) {
-        return new BannerDto(title, bannerUrl);
+    public static BannerDto of(String title, String bannerUrl, String redirectUrl) {
+        return new BannerDto(title, bannerUrl, redirectUrl);
     }
 }
