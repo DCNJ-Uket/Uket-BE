f4063e66d14a6d146c5a77d2ee22acec12a82b2a
sam971114 <simonana@naver.com>
2024-12-25 07:47:27 +0900

    feat: test code에 entity 추가

---
+++ application/ticket-app-api/src/main/resources/db/migration/V2__add_account_and_update_events.sql
@@ -0,0 +1,20 @@
+ALTER TABLE events DROP COLUMN deposit_url;
+
+-- Create Account table
+CREATE TABLE account (
+                         account_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                         deposit_url VARCHAR(255) NOT NULL,
+                         account_number VARCHAR(50),
+                         account_owner VARCHAR(100),
+                         ticket_price INT,
+                         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+                         modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL ON UPDATE CURRENT_TIMESTAMP
+);
+
+-- Add account_id column to Events table
+ALTER TABLE events ADD COLUMN account_id BIGINT;
+
+-- Add foreign key constraint between Events and Account
+ALTER TABLE events
+    ADD CONSTRAINT fk_events_account
+        FOREIGN KEY (account_id) REFERENCES account(account_id);
