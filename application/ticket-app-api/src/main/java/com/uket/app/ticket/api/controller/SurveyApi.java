90950d5c6547efa6b565215a829984dd0f59235b
sam971114 <simonana@naver.com>
2024-12-28 22:00:03 +0900

    feat: swagger 예외 추가

--- application/ticket-app-api/src/main/java/com/uket/app/ticket/api/controller/SurveyApi.java
+++ application/ticket-app-api/src/main/java/com/uket/app/ticket/api/controller/SurveyApi.java
@@ -28,6 +28,35 @@ public interface SurveyApi {
 
     @PostMapping
     @Operation(summary = "질의응답 사용자 응답 제출 API", description = "질의응답 문항에 대해 사용자가 응답한 내용을 등록합니다.")
+    @ApiResponse(responseCode = "404", description = "NOT FOUND", content = @Content(
+        mediaType = "application/json",
+        examples = {
+            @ExampleObject(name = "FO0001", description = "질문지를 찾을 수 없는경우에 발생하는 오류입니다.",
+                value = """
+                                    {"code": "FO0001", "message": "해당 질문지를 찾을 수 없습니다. 질문지 아이디를 다시 확인해주세요."}
+                                    """
+            ),
+            @ExampleObject(name = "US0001", description = "해당 유저를 찾을 수 없는 경우에 발생하는 오류입니다.",
+                value = """
+                                    {"code": "US0001", "message": "해당 사용자를 찾을 수 없습니다."}
+                                    """
+            ),
+            @ExampleObject(name = "FO0003", description = "질문에 대한 응답이 없는경우에 발생합니다.",
+                value = """
+                                    {"code": "FO0003", "message": "해당 질문에 대한 응답이 없습니다."}
+                                    """
+            )
+        }, schema = @Schema(implementation = ErrorResponse.class)))
+    @ApiResponse(responseCode = "400", description = "BAD REQUEST", content = @Content(
+        mediaType = "application/json",
+        examples = {
+            @ExampleObject(name = "FO0006",
+                description = "질문에 대한 응답 목록이 존재하지 않을 경우 발생합니다.",
+                value = """
+                                    {"code": "FO0006", "message": "질문에 대한 응답이 목록에 없습니다."}
+                                    """
+            )
+        }, schema = @Schema(implementation = ErrorResponse.class)))
     ResponseEntity<SurveyAnswerResponse> makeSurveyResponse(
         @Parameter(hidden = true)
         @LoginUserId
