7d840e0cb158a9e56293a4d3eee48a8e1f583244
sam971114 <simonana@naver.com>
2024-12-29 16:49:02 +0900

    feat: 불필요 event repository 로직 삭제

--- domain/event-domain/src/main/java/com/uket/domain/event/repository/EventRepository.java
+++ domain/event-domain/src/main/java/com/uket/domain/event/repository/EventRepository.java
@@ -7,6 +7,4 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface EventRepository extends JpaRepository<Events, Long>, EventRepositoryCustom {
-    @Query("SELECT e FROM Events e JOIN FETCH e.survey WHERE e.id = :eventId")
-    Optional<Events> findEventWithSurvey(@Param("eventId") Long eventId);
 }
