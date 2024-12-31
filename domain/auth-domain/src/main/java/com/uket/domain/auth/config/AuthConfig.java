cb7ceaab720b6d45279df61bd982547113504f80
0703kyj <kyj0703@konkuk.ac.kr>
2024-12-31 21:10:23 +0900

    hotfix: 약관 API 회원가입 예외 처리

--- domain/auth-domain/src/main/java/com/uket/domain/auth/config/AuthConfig.java
+++ domain/auth-domain/src/main/java/com/uket/domain/auth/config/AuthConfig.java
@@ -22,6 +22,7 @@ public class AuthConfig implements WebMvcConfigurer {
                 .excludePathPatterns("/api/v1/dev/**")
                 .excludePathPatterns("/api/v1/email/**")
                 .excludePathPatterns("/api/v1/auth/**")
+                .excludePathPatterns("/api/v1/terms/**")
                 .excludePathPatterns("/api/v1/users/register")
                 .excludePathPatterns("/api/v1/universities","/api/v1/universities/{id}/event","/api/v1/universities/certification")
                 .excludePathPatterns("/swagger-resources/**", "/swagger-ui/**", "/v3/api-docs", "/error");
