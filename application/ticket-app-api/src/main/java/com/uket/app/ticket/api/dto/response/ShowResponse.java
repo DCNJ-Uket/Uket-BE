fa7a36e1e7b28e9ec91fa1c430ce38f51b55f689
nonaninona <53222283+nonaninona@users.noreply.github.com>
2025-01-09 11:03:43 +0900

    [SVR-81] 공연 목록, 정보, 예매 화면에서 축제명도 전달하도록 수정 (#99)
    * feat : 대학 목록 호출 시, 공연 이름도 함께 제공하도록 수정
    
    * feat : show 목록 호출 시, 공연 이름도 함께 제공하도록 수정

--- application/ticket-app-api/src/main/java/com/uket/app/ticket/api/dto/response/ShowResponse.java
+++ application/ticket-app-api/src/main/java/com/uket/app/ticket/api/dto/response/ShowResponse.java
@@ -7,10 +7,11 @@ import java.util.List;
 public record ShowResponse(
         String reservationUserType,
         String universityName,
+        String eventName,
         List<ShowDto> shows
 ) {
 
-    public static ShowResponse of(ReservationUserType reservationUserType, String university, List<ShowDto> shows) {
-        return new ShowResponse(reservationUserType.getValue(), university, shows);
+    public static ShowResponse of(ReservationUserType reservationUserType, String university, String event, List<ShowDto> shows) {
+        return new ShowResponse(reservationUserType.getValue(), university, event, shows);
     }
 }
