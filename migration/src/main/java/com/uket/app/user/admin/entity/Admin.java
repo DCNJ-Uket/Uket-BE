4276e81aac909b3398c7e51cdcde95b6970d292e
nonaninona <starcraft0529@gmail.com>
2025-03-05 18:14:22 +0900

    feat : Admin과 University 관계 추가(ManyToOne)

--- migration/src/main/java/com/uket/app/user/admin/entity/Admin.java
+++ migration/src/main/java/com/uket/app/user/admin/entity/Admin.java
@@ -1,6 +1,7 @@
 package com.uket.app.user.admin.entity;
 
 import com.uket.app.user.admin.enums.AdminRole;
+import com.uket.domain.university.entity.University;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -8,6 +9,8 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -25,6 +28,10 @@ public class Admin {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "admin_id")
     private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "university_id")
+    private University university;
 
     @Enumerated(EnumType.STRING)
     private AdminRole role;
