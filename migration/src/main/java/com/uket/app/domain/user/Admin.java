02d5e42f15e22928e34b04cfb2ed0ad7ba1e39d6
nonaninona <53222283+nonaninona@users.noreply.github.com>
2025-03-05 16:08:51 +0900

    feat : migration 모듈에 Admin, AdminRole 추가 (#112)

---
+++ migration/src/main/java/com/uket/app/domain/user/Admin.java
@@ -0,0 +1,35 @@
+package com.uket.app.domain.user;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Admin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "admin_id")
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private AdminRole role;
+
+    private String email;
+    private String password;
+    private String name;
+    private Boolean isRegistered;
+}
