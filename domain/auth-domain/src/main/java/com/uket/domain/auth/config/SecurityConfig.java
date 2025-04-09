6a53abf5289486a86e71361166cb021d4272ce74
sam971114 <simonana@naver.com>
2025-04-09 17:07:06 +0900

    feat: swagger 문서 접근 허용

--- domain/auth-domain/src/main/java/com/uket/domain/auth/config/SecurityConfig.java
+++ domain/auth-domain/src/main/java/com/uket/domain/auth/config/SecurityConfig.java
@@ -81,8 +81,8 @@ public class SecurityConfig {
                 )
                 .authorizeHttpRequests(registry -> registry // actuator, rest docs 경로, 실무에서는 상황에 따라 적절한 접근제어 필요
                         .requestMatchers("/actuator/*").permitAll()
-                        .requestMatchers("/swagger-ui.html").permitAll()
-                        .requestMatchers("/swagger-ui/**").permitAll()
+                        .requestMatchers("/before-swagger-ui.html").permitAll()
+                        .requestMatchers("/before-swagger-ui/**").permitAll()
                         .requestMatchers("/v3/api-docs/**").permitAll()
                 )
                 .authorizeHttpRequests(registry -> registry
